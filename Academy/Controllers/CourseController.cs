842081aa0e2cf10d5bf4e2d0c447efe651bd0760
intizarbsh <intizarbsh@code.edu.az>
2026-05-12 00:37:13 +0400

    added videos

--- Academy/Controllers/CourseController.cs
+++ Academy/Controllers/CourseController.cs
@@ -26,21 +26,8 @@ namespace Academy.Controllers
         // Detail səhifə
         public async Task<IActionResult> Detail(int id)
         {
-            var course = await _context.Courses
-                .Where(x => x.Id == id)
-                .Select(x => new CourseDetailVM
-                {
-                    Title = x.Title,
-                    Description = x.Description,
-                    Price = x.Price,
-                    ImageUrl = x.ImageUrl,
-                    LanguageName = x.Language.Name,
-                    CategoryName = x.Category.Name,
-                    InstructorName = x.Instructor.FullName,
-                    Duration = x.Duration
-                })
-                .FirstOrDefaultAsync();
-
+            var course = await _service.GetByIdAsync(id);
+            if (course == null) return NotFound();
             return View("~/Views/CourseDetail/Index.cshtml", course);
         }
     }
